76d5d50afd857773af2a1f5110d7d554a1aa750e
root <root@DESKTOP-1423FGT.localdomain>
2024-12-26 00:33:26 -0800

    Remove StringReader method for Scanning

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+/*
+ * TODOs:
+ * Add error reporting feature
+ * Add support for interactive session
+ */
 public class Main {
   public static void main(String[] args) {
     if (args.length < 2) {
--- src/Scanner.java
+++ src/Scanner.java
@@ -1,85 +1,106 @@
 import java.util.ArrayList;
 import java.io.IOException;
-import java.io.StringReader;
 
 public class Scanner {
     public final String source;
     public final ArrayList<Token> tokenList = new ArrayList<>();
 
-    private StringReader reader;
+    private int start;
+    private int current;
     private char currChar;
     private int currLineNum;
 
+    private boolean fileEnd = false;
+
     public Scanner(String source) {
         this.source = source;
+        this.start = 0;
+        this.current = 0;
     }
 
     public void scanTokens() {
-        reader = new StringReader(source);
-        int readValue;
         currLineNum = 1;
-
-        try {
-            while ((readValue = reader.read()) != -1) {
-                currChar = (char) readValue;
-                switch (currChar) {
-                    case '{':
-                        addToken(TokenType.LEFT_BRACE, currChar);
-                        break;
-                    case '}':
-                        addToken(TokenType.RIGHT_BRACE, currChar);
-                        break;
-                    case '(':
-                        addToken(TokenType.LEFT_PAREN, currChar);
-                        break;
-                    case ')':
-                        addToken(TokenType.RIGHT_PAREN, currChar);
-                        break;
-                    case ',':
-                        addToken(TokenType.COMMA, currChar);
-                        break;
-                    case '.':
-                        addToken(TokenType.DOT, currChar);
-                        break;
-                    case '-':
-                        addToken(TokenType.MINUS, currChar);
-                        break;
-                    case '+':
-                        addToken(TokenType.PLUS, currChar);
-                        break;
-                    case '*':
-                        addToken(TokenType.STAR, currChar);
-                        break;
-                    case ';':
-                        addToken(TokenType.SEMICOLON, currChar);
-                        break;
-                    case '\n':
-                        currLineNum++;
-                    case '\r':  //Handle carriage return (CR) ASCII 13 for windows written Lox files
-                    case '\s':
-                        continue;
-                    default:
-                        System.out.println("Error character: " + (int) currChar);
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("Encountered IOException in Scanner: \n" + e.getMessage());
+        while (current < source.length()) {    //if we've reached the end of the file
+            start = current;
+            scanNextToken();
         }
 
         //Add EOF token
-        ++currLineNum;
-        addToken(TokenType.EOF, "", null);
+        tokenList.add(new Token(TokenType.EOF, "", null, currLineNum));
+        fileEnd = true;
     }
 
-    public void addToken(TokenType token, char lexeme) {
-        addToken(token, String.valueOf(lexeme), "null");
+    public void scanNextToken() {
+        char currChar = advanceCurrent();
+        switch (currChar) {
+            case '{':
+                addToken(TokenType.LEFT_BRACE);
+                break;
+            case '}':
+                addToken(TokenType.RIGHT_BRACE);
+                break;
+            case '(':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case ')':
+                addToken(TokenType.RIGHT_PAREN);
+                break;
+            case ',':
+                addToken(TokenType.COMMA);
+                break;
+            case '.':
+                addToken(TokenType.DOT);
+                break;
+            case '-':
+                addToken(TokenType.MINUS);
+                break;
+            case '+':
+                addToken(TokenType.PLUS);
+                break;
+            case '*':
+                addToken(TokenType.STAR);
+                break;
+            case ';':
+                addToken(TokenType.SEMICOLON);
+                break;
+            case '!':
+                addToken(matchNextChar('=') ? TokenType.NOT_EQUAL : TokenType.NOT);
+                break;
+            case '>':
+                addToken(matchNextChar('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+                break;
+            case '<':
+                addToken(matchNextChar('=') ? TokenType.LESSER_EQUAL : TokenType.LESSER);
+                break;
+            case '=':
+                addToken(matchNextChar('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                break;
+            case '\n':
+                currLineNum++;
+            case '\r': //Handle carriage return (CR) ASCII 13 for windows written Lox files
+            case '\s':
+                break;
+            default:
+                System.out.println(
+                        "Error character \'" + currChar + "\' (ASCII: " + (int) currChar + ") at line " + currLineNum);
+        }
+    }
+    
+    private char advanceCurrent() {
+        return source.charAt(current++);
     }
 
-    public void addToken(TokenType token, String lexeme) {
-        addToken(token, lexeme, "null");
+    private boolean matchNextChar(char c) {
+        return source.charAt(current) == c;
     }
-    public void addToken(TokenType token, String lexeme, Object literal) {
-        tokenList.add(new Token(token, lexeme, literal, currLineNum));
+
+    public void addToken(TokenType token) {
+        addToken(token, null);
+    }
+
+    public void addToken(TokenType token, Object literal) {
+        //The lexeme is a substring of source, its start and end position as a substring tracked by start and current
+        tokenList.add(new Token(token, source.substring(start, current), literal, currLineNum));
     }
     
     public void printTokens() {
